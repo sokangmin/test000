1646d45477921bd0b7098b3d7ff50c11dee23fba
sokangmin <sokangmin@gmail.com>
2018-07-16 16:30:47 +0900

    update server.js #16

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var os = require('os');
 var handleRequest = function(request, response) {
 	response.writeHead(200);
-	response.end(os.hostname() + " Hello Kubernetes testv4!");
+	response.end(os.hostname() + " Hello Kubernetes test#16!");
 }
 var server = http.createServer(handleRequest);
 server.listen(8080);
